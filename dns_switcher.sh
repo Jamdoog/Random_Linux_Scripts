3ce76e19446dc64d63c5377693a74cc948fff913
Jamdoog <13838902+Jamdoog@users.noreply.github.com>
2020-08-27 17:26:19 +0100

    Added jamdoog.download

--- dns_switcher.sh
+++ dns_switcher.sh
@@ -36,6 +36,8 @@ www      IN  A       $LB2
   rm file.txt
   rm /var/named/jamdoog.forward
   mv jamdoog.forward /var/named
+
+
   systemctl restart named
 fi
 
@@ -55,5 +57,58 @@ www	 IN  A       $LB1
   rm file.txt
   rm /var/named/jamdoog.forward
   mv jamdoog.forward /var/named
+  systemctl restart named
+fi
+
+# Grabbing jamdoog.download zone
+# Then will increment serial by one so it will push to slaves
+
+SERIALDL=`sed -n '3p' < /var/named/jamdoogdl.forward`
+((SERIALDL=SERIALDL+1))
+< /var/named/jamdoogdl.forward sed  "3a \\
+         $SERIALDL
+" > file.txt
+sed '3d' file.txt > jamdoogdl.forward
+rm file.txt
+
+# Modifying static lines which represent @ and www
+
+if [ $1 != $LB1 ]
+then
+  < jamdoogdl.forward sed "13a \\
+@        IN  A       $LB2
+" > file.txt
+  rm jamdoogdl.forward
+  < file.txt sed "14a \\
+www	 IN  A       $LB2
+" > jamdoogdl.forward
+  rm file.txt
+  sed '13d' jamdoogdl.forward > file.txt
+  rm jamdoogdl.forward
+  sed '15d' file.txt > jamdoogdl.forward
+  rm file.txt
+  rm /var/named/jamdoogdl.forward
+  mv jamdoogdl.forward /var/named
+
+
+  systemctl restart named
+fi
+
+if [ $1 == $LB1 ]
+then
+  < jamdoogdl.forward sed "13a \\
+@        IN  A       $LB1
+" > file.txt
+  rm jamdoogdl.forward
+  < file.txt sed "14a \\
+www	 IN  A       $LB1
+" > jamdoogdl.forward
+  rm file.txt
+  sed '13d' jamdoogdl.forward > file.txt
+  rm jamdoogdl.forward
+  sed '15d' file.txt > jamdoogdl.forward
+  rm file.txt
+  rm /var/named/jamdoogdl.forward
+  mv jamdoogdl.forward /var/named
   systemctl restart named
 fi
